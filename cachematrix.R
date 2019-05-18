77b0d3a4d12e6def3f8937d882e0c41079cbcc1d
mgilangs <50157753+mgilangs@users.noreply.github.com>
2019-05-18 14:45:15 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invrs <- NULL
+  set <- function(y){
+    x <<- y
+    invrs <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) invrs <<- solveMatrix
+  getInverse <- function() invrs
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
+
+
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invrs <- x$getInverse()
+  if(!is.null(invrs)){
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data)
+  x$setInverse(invrs)
+  invrs
 }
